9a1d152d1f965d042caeceef7e86deb4683da329
jarydh <jaryd.hodge@gmail.com>
2025-01-09 14:24:14 -0800

    logic is sound but it errors out

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,24 @@
 #include "SPIFFS.h"
+#include <AccelStepper.h>
+#include <MultiStepper.h>
+#include "stepper_control.h"
+#include "inverse_kin.h"
+
+#define LED_PIN 22
+#define STEP_PIN_1 12
+#define DIR_PIN_1 14
+#define STEP_PIN_2 26
+#define DIR_PIN_2 27
+#define STEPS_PER_REV 200
+#define STEPPER_GEAR_RATIO 1.8
+
+#define START_POSITION -1, 0
+#define FILE_NAME "/positions.in"
+
+AccelStepper stepper1(AccelStepper::DRIVER, STEP_PIN_1, DIR_PIN_1);
+AccelStepper stepper2(AccelStepper::DRIVER, STEP_PIN_2, DIR_PIN_2);
+MultiStepper steppers;
+#include "SPIFFS.h"
 // #include "stepper_control.h"
 #include "WiFi.h"
 #include "WifiManager.h"
@@ -19,6 +39,52 @@ AsyncWebServer server(80);
 
 #define START_POSITION -1, 0
 #define FILE_NAME "/positions.in"
+
+long *angles_to_steps(MotorAngles angles)
+{
+  // recalculate a1 to the closest equivalent angle
+  if (angles.a1 - stepper1.currentPosition() > 180)
+  {
+    angles.a1 -= 360;
+  }
+  if (stepper1.currentPosition() - angles.a1 > 180)
+  {
+    angles.a1 += 360;
+  }
+  assert(abs(stepper1.currentPosition() - angles.a1) <= 180);
+
+  long pos_1 = round(angles.a1);
+  long delta = pos_1 - stepper1.currentPosition();
+
+  double drift = delta / STEPPER_GEAR_RATIO;
+
+  long pos_2 = round(drift + angles.a2);
+
+  static long steps[2];
+  steps[0] = pos_1;
+  steps[1] = pos_2;
+
+  return steps;
+}
+
+// sends the arm head to the x, y position
+void go_to(double x, double y)
+{
+  Serial.printf("go to [%0.2f, %0.2f]\n", x, y);
+  MotorAngles angles = coord_to_angles(x, y);
+  Serial.printf("\tneed angles: %.0f°, %.0f°\n", angles.a1, angles.a2);
+  long *steps = angles_to_steps(angles);
+
+  Serial.printf("\tsend arm 1 to %d\n", steps[0]);
+  Serial.printf("\tsend arm 2 to %d\n", steps[1]);
+
+  steppers.moveTo(steps);
+  steppers.runSpeedToPosition();
+
+  // reset the motors frame of reference
+  stepper1.setCurrentPosition(round(positiveMod(angles.a1, 360)));
+  stepper2.setCurrentPosition(round(positiveMod(angles.a2, 360)));
+}
 
 void setup()
 {
@@ -81,36 +147,34 @@ void setup()
                 if (ch == ',')
                 {
                   x = body.substring(i, j).toDouble();
-                  // Serial.printf("found '%c' at %d. Slicing out %.4lf\n", ch, j, x);
                   i = j + 1;
                 }
                 if (ch == '\n' || ch == '\0')
                 {
                   y = body.substring(i, j).toDouble();
-                  // Serial.printf("found '%c' at %d. Slicing out %.4lf\n", ch, j, y);
                   i = j + 1;
                   Serial.printf("%lf,%lf\n", x, y);
+                  go_to(x, y);
+                  delay(500);
                 }
                 j++;
               }
-              // Create a buffer to store the body data as a string
-              // char *buf = (char*)malloc(len + 1);
-              // memcpy(buf, data, len);
-              // buf[len] = '\0';
-
-              // Print the received data
-              // Serial.print("Received body data: ");
-              // Serial.println(buf);
-              // Serial.print("Chunk index: ");
-              // Serial.println(index);
-              // Serial.print("Total length: ");
-              // Serial.println(total);
-
-              // free(buf);
-            });
+              go_to(START_POSITION); });
 
   // Start server
   server.begin();
+
+  // stepper stuff
+  //
+  //
+  //
+  stepper1.setMaxSpeed(80);
+  stepper1.setSpeed(80);
+  stepper2.setMaxSpeed(80);
+  stepper2.setSpeed(80);
+
+  steppers.addStepper(stepper1);
+  steppers.addStepper(stepper2);
 }
 
 void loop() {}
--- src/stepper_control.cpp
+++ src/stepper_control.cpp
@@ -1,117 +1,31 @@
-#include "SPIFFS.h"
-#include <AccelStepper.h>
-#include <MultiStepper.h>
-#include "stepper_control.h"
-#include "inverse_kin.h"
 
-#define LED_PIN 22
-#define STEP_PIN_1 12
-#define DIR_PIN_1 14
-#define STEP_PIN_2 26
-#define DIR_PIN_2 27
-#define STEPS_PER_REV 200
-#define STEPPER_GEAR_RATIO 1.8
 
-#define START_POSITION -1, 0
-#define FILE_NAME "/positions.in"
+// // executed on startup after setup() as a script
+// void execute()
+// {
+//     File f = SPIFFS.open(FILE_NAME);
+//     if (!f.size() > 0)
+//     {
+//         Serial.printf("❌\tFailed to open file '%s'\n", FILE_NAME);
+//         return;
+//     }
 
-AccelStepper stepper1(AccelStepper::DRIVER, STEP_PIN_1, DIR_PIN_1);
-AccelStepper stepper2(AccelStepper::DRIVER, STEP_PIN_2, DIR_PIN_2);
-MultiStepper steppers;
+//     while (f.available())
+//     {
+//         String line = f.readStringUntil('\n');
+//         double x, y;
 
-long *angles_to_steps(MotorAngles angles)
-{
-    // recalculate a1 to the closest equivalent angle
-    if (angles.a1 - stepper1.currentPosition() > 180)
-    {
-        angles.a1 -= 360;
-    }
-    if (stepper1.currentPosition() - angles.a1 > 180)
-    {
-        angles.a1 += 360;
-    }
-    assert(abs(stepper1.currentPosition() - angles.a1) <= 180);
+//         if (sscanf(line.c_str(), "%lf,%lf", &x, &y) == 2)
+//         {
+//             assert(x * x + y * y <= 1);
+//             go_to(x, y);
+//             delay(200);
+//         }
+//         else
+//         {
+//             Serial.printf("Skipping invalid line: %s\n", line.c_str());
+//         }
+//     }
+//     f.close();
+// }
 
-    long pos_1 = round(angles.a1);
-    long delta = pos_1 - stepper1.currentPosition();
-
-    double drift = delta / STEPPER_GEAR_RATIO;
-
-    long pos_2 = round(drift + angles.a2);
-
-    static long steps[2];
-    steps[0] = pos_1;
-    steps[1] = pos_2;
-
-    return steps;
-}
-
-// sends the arm head to the x, y position
-void go_to(double x, double y)
-{
-    Serial.printf("go to [%0.2f, %0.2f]\n", x, y);
-    MotorAngles angles = coord_to_angles(x, y);
-    Serial.printf("\tneed angles: %.0f°, %.0f°\n", angles.a1, angles.a2);
-    long *steps = angles_to_steps(angles);
-
-    Serial.printf("\tsend arm 1 to %d\n", steps[0]);
-    Serial.printf("\tsend arm 2 to %d\n", steps[1]);
-
-    steppers.moveTo(steps);
-    steppers.runSpeedToPosition();
-
-    // reset the motors frame of reference
-    stepper1.setCurrentPosition(round(positiveMod(angles.a1, 360)));
-    stepper2.setCurrentPosition(round(positiveMod(angles.a2, 360)));
-}
-
-// executed on startup after setup() as a script
-void execute()
-{
-    File f = SPIFFS.open(FILE_NAME);
-    if (!f.size() > 0)
-    {
-        Serial.printf("❌\tFailed to open file '%s'\n", FILE_NAME);
-        return;
-    }
-
-    while (f.available())
-    {
-        String line = f.readStringUntil('\n');
-        double x, y;
-
-        if (sscanf(line.c_str(), "%lf,%lf", &x, &y) == 2)
-        {
-            assert(x * x + y * y <= 1);
-            go_to(x, y);
-            delay(200);
-        }
-        else
-        {
-            Serial.printf("Skipping invalid line: %s\n", line.c_str());
-        }
-    }
-    f.close();
-}
-
-void init_steppers()
-{
-    stepper1.setMaxSpeed(80);
-    stepper1.setSpeed(80);
-    stepper2.setMaxSpeed(80);
-    stepper2.setSpeed(80);
-
-    steppers.addStepper(stepper1);
-    steppers.addStepper(stepper2);
-
-    Serial.println("\n\n🐸🐸🐸🐸🐸🐸🐸🐸🐸🐸🐸🐸🐸🐸🐸🐸🐸🐸🐸🐸🐸🐸🐸🐸🐸🐸🐸🐸\n\n");
-    Serial.println("done setup, executing movements");
-
-    execute();
-
-    Serial.println("done executing movements.");
-    delay(2000);
-    Serial.println("resetting");
-    go_to(START_POSITION);
-    Serial.println("\n\n🐸🐸🐸🐸🐸🐸🐸🐸🐸🐸🐸🐸🐸🐸🐸🐸🐸🐸🐸🐸🐸🐸🐸🐸🐸🐸🐸🐸\n\n");
-}
